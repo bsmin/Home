fd8525a13eb7e047f0d7741cf23ab67343a176a1
emma_sweet@naver.com <rkdlem1!>
2018-01-05 02:40:26 +0900

    중간 저장 세번째 저장 Commit

--- app/src/main/java/com/example/emma/fish/view/Main.java
+++ app/src/main/java/com/example/emma/fish/view/Main.java
@@ -17,7 +17,7 @@ public class Main  extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main,container,false);
+        return inflater.inflate(R.layout.fragment_main,container,false);
     }
 
     @Override
